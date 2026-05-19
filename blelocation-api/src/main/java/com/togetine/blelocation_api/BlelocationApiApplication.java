90e64342f7f3fdb6a3ddbd4cae6b692ad794e3c0
togawa427 <ssinya0922@gmail.com>
2026-05-19 22:28:10 +0900

    feat: SpringBootのプロジェクト作成

---
+++ blelocation-api/src/main/java/com/togetine/blelocation_api/BlelocationApiApplication.java
@@ -0,0 +1,13 @@
+package com.togetine.blelocation_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlelocationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlelocationApiApplication.class, args);
+	}
+
+}
---
+++ blelocation-api/src/test/java/com/togetine/blelocation_api/BlelocationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.togetine.blelocation_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlelocationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
